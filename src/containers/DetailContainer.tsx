184af77c4d8c04cacec048aac8af2949fc5a2f3a
최진호 <jh.choi@airpremia.com>
2020-11-15 00:37:04 +0900

    수정페이지와 상세페이지 리로딩 문제 해결

--- src/containers/DetailContainer.tsx
+++ src/containers/DetailContainer.tsx
@@ -1,13 +1,17 @@
-import React, { useCallback } from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import Detail from '../components/Detail';
 import { logout as logoutSaga } from '../redux/modules/auth';
 import {useParams} from "react-router-dom";
 import {RootState} from "../redux/modules/rootReducer";
-import {message as messageDialog} from "antd";
 import {push} from "connected-react-router";
+import {getBooksList} from "../redux/modules/books";
 
 const DetailContainer = () => {
+  // [project] saga 함수를 실행하는 액션 생성 함수를 실행하는 함수를 컨테이너에 작성했다.
+  // [project] 컨테이너에서 useDispatch, useSelector, useCallback 을 활용해서 중복없이 비동기 데이터를 보여주도록 처리했다.
+  // [project] Edit 나 Detail 컴포넌트에서 새로고침 시, 리스트가 없는 경우, 리스트를 받아오도록 처리했다.
+
   const dispatch = useDispatch();
   const logout = useCallback(() => {
     dispatch(logoutSaga());
@@ -19,22 +23,22 @@ const DetailContainer = () => {
   const {id} = useParams();
   const id2 = parseInt(id);
 
-  const { books, error } = useSelector((state: RootState) => state.books);
+  const { books, loading, error } = useSelector((state: RootState) => state.books);
+
+  useEffect(() => {
+    if (books) return;
+    dispatch(getBooksList());
+  }, [books, dispatch]);
+
   if (books == null)
     return null;
-  const book = books.find((book) => book.bookId === id2);
-  if (id === undefined) {
-    messageDialog.error('Please fill out all inputs');
-    return null;
-  }
-  // [project] saga 함수를 실행하는 액션 생성 함수를 실행하는 함수를 컨테이너에 작성했다.
-  // [project] 컨테이너에서 useDispatch, useSelector, useCallback 을 활용해서 중복없이 비동기 데이터를 보여주도록 처리했다.
-  // [project] Edit 나 Detail 컴포넌트에서 새로고침 시, 리스트가 없는 경우, 리스트를 받아오도록 처리했다.
 
-  return (error
-      ? <p style={{textAlign: "center"}}>에러 발생!...</p>
-      : <Detail book={book} logout={logout} editBook={editBook}/>
-  );
+  const book = books.find((book) => book.bookId === id2);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!book) return null;
+  return <Detail book={book} logout={logout} editBook={editBook}/>
 };
 
 export default DetailContainer;
